053c3edf9fa531d1534642aba0f71ca1abcf56f8
Simon Waiblinger <SimonWaiblinger@live.at>
2022-08-04 20:43:19 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,54 @@
+import path from 'node:path'
+import { writeFileSync } from 'node:fs'
+import axios from 'axios'
+import sharp from 'sharp'
+
+// Here enmap could be used to build quickly a persistant counter, which doesn't vanish on restart
+let fileCounter: number = 0
+
+async function downloadFile(myUrl: string, downloadFolder: string | undefined = path.join(__dirname, 'images/image-' + fileCounter + '.jpg')) {
+  // Get the file name
+  // The path of the downloaded file on our machine
+  const localFilePath = path.resolve(__dirname, downloadFolder)
+  const response = await axios({
+    method: 'GET',
+    url: myUrl,
+    responseType: 'arraybuffer'
+  })
+
+  console.log(response.data)
+  writeFileSync(localFilePath, response.data)
+  console.log('Successfully downloaded file!')
+  fileCounter++
+}
+
+type Coordinates = { xStart: number, xEnd: number, yStart: number, yEnd: number }
+type ExtractParameters = { left: number, top: number, width: number, height: number }
+
+async function cropImage(imageUrl: string, coordinates: Coordinates, downloadFilePath: string = 'images/image-' + fileCounter + '.jpg', cropFilePath = path.join(__dirname, 'images/crop-' + fileCounter + '.jpeg')) {
+  // Convert coordinates to extract parameters
+  const extractParameters: ExtractParameters = { left: coordinates.xStart, top: coordinates.yStart, width: coordinates.xEnd - coordinates.xStart, height: coordinates.yEnd - coordinates.yStart }
+
+  const localFilePath = path.resolve(__dirname, downloadFilePath)
+  console.log('File path', localFilePath)
+
+  //Download image
+  await downloadFile(imageUrl)
+
+  // Throw error when extractParameters are not correct
+  const imageMetadata = await sharp(localFilePath).metadata()
+  console.log('Image size', imageMetadata)
+
+  if ((imageMetadata.width as number) < extractParameters.width || (imageMetadata.height as number) < extractParameters.height) {
+    throw (new Error('Image is not big enough for the provided coordinates'))
+  }
+
+  sharp(localFilePath)
+    .extract(extractParameters)
+    .jpeg()
+    .toFile(cropFilePath, error => { if (error) console.error('Cropping failed\n', error) })
+}
+
+// TODO add file extension recognition in wrapping function
+(async () => await cropImage('http://www.usefulcraft.com/wp-content/uploads/2019/12/4knaruto-17-scaled.jpg', { xStart: 100, xEnd: 200, yStart: 100, yEnd: 200 })
+  .catch(error => console.error('Crop image failed\n', error)))()
